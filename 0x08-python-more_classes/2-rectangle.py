6103d76653e9feaf6c3804a79fb95b819f8e9912
2005-annabel <owangeshi@gmail.com>
2023-09-16 13:27:29 -0700

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -22,9 +22,9 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value)is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if(value)< 0:
+        if (value) < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -37,14 +37,15 @@ class Rectangle:
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if(value) < 0:
+        if (value) < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
     def area(self):
         return self.__width * self.__height
 
     def perimeter(self):
         perim = (self.__height + self.__width)*2
-        if(self.__width == 0 or self.__height == 0):
+        if (self.__width == 0 or self.__height == 0):
             perim = 0
         return perim
