e57fbd37135071ad061aeeacc9b9db1601f47fa2
2005-annabel <owangeshi@gmail.com>
2023-09-16 16:59:06 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""The function below defines an object attribute lookup."""
+
+
+def lookup(obj):
+    """Return action is that of a list of the objects attributes."""
+    return (dir(obj))
