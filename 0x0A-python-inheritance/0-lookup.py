9e418591ed1a98ac31edce912cf3d7a35231f532
2005-annabel <125447128+2005-annabel@users.noreply.github.com>
2023-10-03 11:59:42 -0700

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""The function below defines an object attribute lookup."""
-
-
-def lookup(obj):
-    """Return action is that of a list of the objects attributes."""
-    return (dir(obj))
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""an empty class"""
-
-class BaseGeometry:
-    """empty class"""
-    pass
