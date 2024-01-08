fb268839b3d6285fa1b13afd669f489aa15df956
2005-Annabel <owangeshi@gmail.com>
2024-01-08 12:10:59 -0800

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-function factorial(val) {
-  if (isNaN(val)) {
+function factorial (n) {
+  if (isNaN(n)) {
     return 1;
-  } 
-  if (val === 0 || val === 1) {
+  }
+  if (n === 0 || n === 1) {
     return 1;
-  } 
-  return val * factorial(val - 1);
+  } else {
+    return n * factorial(n - 1);
+  }
 }
-const number = parseInt(process.argv[2]);
-
-console.log(factorial(number));
+const arg = process.argv[2];
+const num = parseInt(arg);
+console.log(factorial(num));
