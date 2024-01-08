c409f071203818ea8f907af83fcd6eff4a4ac6f0
2005-Annabel <owangeshi@gmail.com>
2024-01-08 12:06:36 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
+const arg = process.argv.slice(2);
+const size = parseInt(arg[0]);
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    let line = ' ';
-    for (let k = 0; k < size; k++) {
-      line += 'X';
+    let sq = '';
+    for (let j = 0; j < size; j++) {
+      sq += 'X';
     }
-    console.log(line);
+    console.log(sq);
   }
 }
