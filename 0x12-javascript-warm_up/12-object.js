41b05f8373ced1af3ac6a9603ce06ba8e17a9885
2005-Annabel <owangeshi@gmail.com>
2024-01-08 12:13:54 -0800

    12-object.js

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const numbers = args.map(arg => parseInt(arg));
-if (numbers.length < 2) {
-  console.log(0);
-} else {
-  const sortedNum = numbers.sort((a, b) => b - a);
-  const secondLarg = sortedNum[1];
-  if (secondLarg === 12) {
-    sortedNum[1] = 89;
-  }
-
-  console.log(sortedNum[1]);
-}
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
