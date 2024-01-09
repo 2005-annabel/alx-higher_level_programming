337d1b00b0c7b8691717267edef0c9e413fca914
2005-Annabel <owangeshi@gmail.com>
2024-01-09 11:31:33 -0800

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,17 +2,17 @@
 
 const Squares = require('./5-square.js');
 class Square extends Squares {
-	charPrint (c) {
-		if (c === undefined) {
-			c = 'X';
-		}
-		for (let i = 0; i < this.height; i++) {
-			let display = '';
-			for (let j = 0; j < this.width; j++) {
-				display += c;
-			}
-			console.log(display);
-		}
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let display = '';
+      for (let j = 0; j < this.width; j++) {
+        display += c;
+      }
+      console.log(display);
+    }
+  }
 }
 module.exports = Square;
