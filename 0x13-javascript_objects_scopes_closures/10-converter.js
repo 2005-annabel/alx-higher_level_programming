71579d1dde28723d2730b9dbbf745bd019130e43
2005-Annabel <owangeshi@gmail.com>
2024-01-09 11:43:14 -0800

    10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  return function convert (number) {
-    // Base case
-    if (number < base) {
-      return number.toString();
-    } else {
-      // Recursive case
-      return convert(Math.floor(number / base)) + (number % base).toString();
-    }
+  return function (newNumber) {
+    return newNumber.toString(base);
   };
 };
