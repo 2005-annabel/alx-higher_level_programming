01696f6c9b77623e6cd578e44ef14fc3339873ec
2005-annabel <owangeshi@gmail.com>
2024-03-16 13:35:08 -0700

    11

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""the script entails:
+- a URL requirement,
+- a request sent to the URL
+"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
